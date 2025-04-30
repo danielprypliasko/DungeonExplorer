fc0f2e4a34d57dde53d0ca82e9f16ee0b6d7579b
daniel <me@daniel.software>
2025-04-30 21:36:17 +0100

    remove small mistake in comment from older code

--- Monster.cs
+++ Monster.cs
@@ -9,7 +9,7 @@ namespace DungeonExplorer
         /// Initializes a new Monster
         /// </summary>
         /// <param name="name">The name of the monster</param>
-        /// <param name="health">The starting and maximum health of the monster</param>
+        /// <param name="health">The health of the monster</param>
         /// <param name="defence">The defence value of the monster</param>
         public Monster(string name, int health, int defence, int strength) : base(name, health, defence, strength)
         {
